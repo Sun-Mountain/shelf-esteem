8b806fc6ecd5a4e323747a90e3b032f11ea0273a
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-01-31 13:26:00 -0500

    add session in client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <h1 className='text-4xl'>Home</h1>;
+import User from "@/components/User";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/authOptions";
+
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  return (
+    <>
+      <h1 className='text-4xl'>Home</h1>
+      <h2>Client Session</h2>
+      <User />
+      <h2>Server Session</h2>
+      {JSON.stringify(session)}
+    </>
+  );
 }
