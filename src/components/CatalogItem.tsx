a18819262b5b64640177cd9c7031ed015ba22600
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-02-06 09:43:45 -0500

    add catalog item

---
+++ src/components/CatalogItem.tsx
@@ -0,0 +1,61 @@
+import { CircularProgress } from '@mui/material';
+import { Check, DoNotDisturb } from '@mui/icons-material';
+
+interface CatalogItemProps {
+  isbn: string;
+  isbnIndex: number;
+  bookData: any[];
+}
+
+const CatalogItem = ({
+  isbn,
+  isbnIndex,
+  bookData
+}: CatalogItemProps) => {
+
+  let book
+
+  if (bookData.length > 0) {
+    if (!!bookData.find((book) => book.isbn === isbn)) {
+      book = bookData.find((book) => book.isbn === isbn);
+    }
+  }
+
+  return (
+    <div className="catalog-item">
+      <div className="item-label">
+        <div className='item-title'>
+          {book ? (
+            book.title ? book.title : isbn
+          ) : (
+            isbn
+          )}
+        </div>
+        {book && (
+          <div className="item-author">
+            {book.authorName ? book.authorName.join(', ') : 'Author not found'}
+          </div>
+        )}
+        {book?.title && (
+          <div className="item-isbn">
+            {book.isbn}
+          </div>
+        )}
+      </div>
+
+      <div className='icon-container'>
+        { book ? (
+          book.found ? (
+            <Check className="icon success" />
+          ) : (
+            <DoNotDisturb className="icon error" />
+          )
+        ) : (
+          <CircularProgress disableShrink />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default CatalogItem
