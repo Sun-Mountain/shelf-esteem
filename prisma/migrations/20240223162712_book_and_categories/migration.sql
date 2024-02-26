eccf437236a0629d29353d881fb56c34af2c7478
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-02-26 17:43:24 -0500

    update packages

---
+++ prisma/migrations/20240223162712_book_and_categories/migration.sql
@@ -0,0 +1,57 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'BETA', 'MOD', 'ADMIN');
+
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'USER';
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("name")
+);
+
+-- CreateTable
+CREATE TABLE "IndustryIdentifier" (
+    "type" TEXT NOT NULL,
+    "identifier" TEXT NOT NULL,
+    "bookId" TEXT,
+
+    CONSTRAINT "IndustryIdentifier_pkey" PRIMARY KEY ("identifier")
+);
+
+-- CreateTable
+CREATE TABLE "BookCategory" (
+    "id" SERIAL NOT NULL,
+    "bookId" TEXT NOT NULL,
+    "categoryName" TEXT NOT NULL,
+
+    CONSTRAINT "BookCategory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" TEXT NOT NULL,
+    "authors" TEXT[],
+    "description" TEXT NOT NULL,
+    "language" TEXT NOT NULL,
+    "maturityRating" TEXT NOT NULL,
+    "pageCount" INTEGER NOT NULL,
+    "publishedDate" TEXT NOT NULL,
+    "publisher" TEXT NOT NULL,
+    "thumbnail" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "addedBy" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Book_id_key" ON "Book"("id");
+
+-- AddForeignKey
+ALTER TABLE "IndustryIdentifier" ADD CONSTRAINT "IndustryIdentifier_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BookCategory" ADD CONSTRAINT "BookCategory_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BookCategory" ADD CONSTRAINT "BookCategory_categoryName_fkey" FOREIGN KEY ("categoryName") REFERENCES "Category"("name") ON DELETE CASCADE ON UPDATE CASCADE;
