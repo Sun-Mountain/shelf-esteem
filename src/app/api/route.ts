900f0747dd60d982e05a34bf3a3caa9b279d3262
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-02-28 08:58:05 -0500

    reset

--- src/app/api/route.ts
+++
@@ -1,9 +0,0 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/authOptions";
-import { NextResponse } from "next/server";
-
-export const GET = async (req: Request) => {
-  const session = await getServerSession(authOptions);
-  
-  return NextResponse.json({ authenticated: !!session })
-};
--- src/helpers/isbnValidators.ts
+++
@@ -1,3 +0,0 @@
-export const validateIsbn = (isbn: string) => {
-
-});
