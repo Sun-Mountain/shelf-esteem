c872b6053dfea638386f263b4333b87231db6f83
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-02-28 13:42:33 -0500

    install next auth

---
+++ src/app/api/user/[id]/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Prisma } from "@prisma/client";
+
+export async function GET(
+  _: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const id = params?.id;
+  const book = await getBook(id);
+  if (!book) {
+    const failedResponse = NextResponse.json(
+      { error: "Ouch, GET is not working my friend" },
+      { status: 404 }
+    );
+    return failedResponse;
+  }
+  return NextResponse.json(book);
+};
