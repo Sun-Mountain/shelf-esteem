5e36b9a1e1256a238450cf5d1beb091c4fc10e0c
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-03-01 12:01:03 -0500

    update permissions

--- src/lib/accessControl.ts
+++ src/lib/accessControl.ts
@@ -3,6 +3,16 @@ import { AccessControl } from 'accesscontrol';
 
 let grantsObject = {
   [Role.USER]: {
+    category: { 'read:any': ['*'] },
+    book: {
+      'create:any': ['*'],
+      'read:any': ['*', '!addedBy', '!createdAt', '!udpatedAt']
+    },
+    userLibraryBook: {
+      'create:own': ['*'],
+      'read:own': ['*'],
+      'delete:own': ['*']
+    }
   },
 };
 
