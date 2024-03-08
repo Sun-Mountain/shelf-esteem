6301a864984909566103f004a3e58b3c8d9d6aa4
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-03-08 13:33:26 -0500

    navigation component

---
+++ src/components/Navigation/MainNav.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import { AutoStoriesTwoTone } from '@mui/icons-material';
+
+const MainNavigation = async () => {
+
+  return (
+    <nav>
+      <div id="main-navigation">
+        <div className="icon-container">
+          <Link href='/'>
+            <AutoStoriesTwoTone className="icon" />
+          </Link>
+        </div>
+        <div className='nav-link-group'>
+          <div>
+            <Link href='/about'>
+              About
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+};
+
+export default MainNavigation;
