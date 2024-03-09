638c41284a27431f96833843b3553b86a9893cc5
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-03-09 18:05:26 -0500

    change variable names

--- src/app/(public)/style/page.tsx
+++ src/app/(public)/style/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import ColorShield from "@components/StyleGuide/ColorShield";
 
 const StyleGuide = () => {
   return (
@@ -6,6 +6,49 @@ const StyleGuide = () => {
       <h1>Style Guide</h1>
       <div>
         <h2>Color Guide</h2>
+        <div className="shields-container">
+          <ColorShield title="base" name="Eggshell" rgbCode="rgb(0, 123, 255)" />
+          <ColorShield title="primary" name="Delft Blue" rgbCode="rgb(102, 79, 92)" lightText={true} />
+          <ColorShield title="secondary" name="Sunset" rgbCode="rgb(242, 204, 143)" />
+          <ColorShield title="tertiary" name="Sage" rgbCode="rgb(186, 191, 149)" />
+          <ColorShield title="accent" name="Neon Blue" rgbCode="rgb(67, 97, 238)" lightText={true} />
+          <ColorShield title="neutral" name="Rose Taupe" rgbCode="rgb(143, 93, 93)" lightText={true} />
+          <div className="color-shield">
+            <div className="color-stripe greyscale1-color white-text">
+              Eerie-Black: rgb(33, 37, 41)
+            </div>
+            <div className="color-stripe greyscale2-color white-text">
+              Onyx: rgb(52, 58, 64)
+            </div>
+            <div className="color-stripe greyscale3-color white-text">
+              Outer-Space: rgb(73, 80, 87)
+            </div>
+            <div className="color-stripe greyscale4-color white-text">
+              Slate-Gray: rgb(108, 117, 125)
+            </div>
+            <div className="color-stripe greyscale5-color">
+              French-Gray-2: rgb(173, 181, 189)
+            </div>
+            <div className="color-stripe greyscale6-color">
+              French-Gray: rgb(206, 212, 218)
+            </div>
+            <div className="color-stripe greyscale7-color">
+              Seasalt: rgb(248, 249, 250)
+            </div>
+            <div className="color-stripe greyscale8-color">
+              Antiflash-White: rgb(233, 236, 239)
+            </div>
+            <div className="color-stripe greyscale9-color">
+              Platinum: rgb(222, 226, 230)
+            </div>
+            <div className="color-stripe greyscale10-color">
+              White: rgb(255, 255, 255)
+            </div>
+            <div className="color-details">
+              <div className="detail title">Shadows</div>
+            </div>
+          </div>
+        </div>
       </div>
       <div>
         <h2>Typography</h2>
