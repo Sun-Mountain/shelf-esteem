2fd136ba30af0050c4bfe60e5e23c9ad01dd1965
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-03-09 16:49:03 -0500

    header styles

---
+++ src/app/(public)/style/page.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const StyleGuide = () => {
+  return (
+    <section>
+      <h1>Style Guide</h1>
+      <div>
+        <h2>Color Guide</h2>
+      </div>
+      <div>
+        <h2>Typography</h2>
+        <div>
+          <h3>Headers</h3>
+          <h1>H1: Roboto, 3rem</h1>
+          <h2>H2: Roboto, 2.75rem</h2>
+          <h3>H3: Roboto, 2.125rem</h3>
+          <h4>H4: Roboto, 2rem</h4>
+          <h5>H5: Roboto, 1.75rem</h5>
+          <h6>H6: Roboto, 1.5rem</h6>
+        </div>
+        <div>
+          <h3>Body Text</h3>
+          <p>Text, Roboto 1.125rem</p>
+        </div>
+      </div>
+      <div>
+        <h2>Buttons</h2>
+      </div>
+      <div>
+        <h2>Forms</h2>
+      </div>
+    </section>
+  )
+}
+
+export default StyleGuide
