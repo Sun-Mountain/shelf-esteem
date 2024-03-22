26c61f4ca911841d1833682160b0959cb13b27c0
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-03-22 15:59:51 -0400

    links on dashboard

--- src/app/admin/dash/page.tsx
+++ src/app/admin/dash/page.tsx
@@ -1,6 +1,11 @@
 'use client';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import {
+  Category,
+  CollectionsBookmark,
+  Group
+} from '@mui/icons-material';
 
 const AdminDash = () => {
   const [userNum, setUserNum] = useState();
@@ -34,16 +39,36 @@ const AdminDash = () => {
   return (
     <section>
       <h1>Admin Dashboard</h1>
-      <div className="">
+      <div className="stat-links">
         <Link href="/admin/users">
-          Users {userNum && (
-            <span>({userNum})</span>
-          )}
+          <div className="icon-container">
+            <Group />
+          </div>
+          <div className="text-container">
+            Users
+            {userNum && (
+              <div className="number">({userNum})</div>
+            )}
+          </div>
         </Link>
         <Link href="/admin/books">
-          Books {booksNum && (
-            <span>({booksNum})</span>
-          )}
+          <div className="icon-container">
+            <CollectionsBookmark />
+          </div>
+          <div className="text-container">
+            Books
+            {booksNum && (
+              <div className="number">({booksNum})</div>
+            )}
+          </div>
+        </Link>
+        <Link href="/admin/categories">
+          <div className="icon-container">
+            <Category />
+          </div>
+          <div className="text-container">
+            Categories
+          </div>
         </Link>
       </div>
     </section>
