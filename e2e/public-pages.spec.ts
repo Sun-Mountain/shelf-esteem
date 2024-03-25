2fe299129129984b0d64535844f28a079ce37598
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-03-25 13:00:58 -0400

    change test

--- e2e/public-pages.spec.ts
+++ e2e/public-pages.spec.ts
@@ -1,17 +1,12 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, type Page } from '@playwright/test';
 
-test('has website title', async ({ page }) => {
-  await page.goto('/');
-  const title = page.locator('h1');
-  await expect(title).toHaveText('Shelf Esteem');
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:4000');
 });
 
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
+test.describe('Home Page',() => {
+  test('should display the title', async ({ page }) => {
+    const title = page.locator('h1');
+    await expect(title).toHaveText('Shelf Esteem');
+  });
+});
